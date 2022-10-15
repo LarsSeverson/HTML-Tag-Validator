4ca94a3654d963ea198d91516d0ae6cbd3cd48ba
servosdesign <servos.design0@gmail.com>
2022-10-15 11:08:04 -0500

    linked list queue class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack s = new Stack();
+        Queue q = new Queue();
+        q.enqueue(10);
+        System.out.println("Q: " + q.front.key);
     }
 }
